8e80cda17418b33edf88010d85f28b90d5566bdb
agstyo <agustiyo95@gmail.com>
2025-04-15 14:53:38 +0700

    add puppter config

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json());
 
 // Inisialisasi WhatsApp client
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      }
 });
 
 
